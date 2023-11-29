f82b842b5734337feb79ef4881014c6474768f01
Pushparaj13811 <hmehta588@rku.ac.in>
2023-11-29 21:12:35 +0530

    Updated progess of Day 13 and 14

--- Day13/mergeSortedArray.java
+++ Day13/mergeSortedArray.java
@@ -1,12 +1,3 @@
-/*
- * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, 
- * representing the number of elements in nums1 and nums2 respectively.Merge nums1 and nums2 into a single array 
- * sorted in non-decreasing order.The final sorted array should not be returned by the function, but instead be 
- * stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements 
- * denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 
- * has a length of n.
- */
-
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int a = m - 1;
@@ -14,10 +5,10 @@ class Solution {
         int aMerge = m + n - 1;
         
         while (b >= 0) {
-            if (a >= 0 && nums1[p1] > nums2[p2]) {
-                nums1[aMerge--] = nums1[p1--];
+            if (a >= 0 && nums1[a] > nums2[b]) {
+                nums1[aMerge--] = nums1[a--];
             } else {
-                nums1[aMerge--] = nums2[p2--];
+                nums1[aMerge--] = nums2[b--];
             }
         }
     }
